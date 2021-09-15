a6bd84a97684b100cd44d2aa0ff217295fa5167a
Grace Ester Alexander <63588513+gealexxx@users.noreply.github.com>
2021-09-15 13:11:16 +0700

    Add files via upload

---
+++ .github/workflows/c.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 26 16:43:49 2019
+
+@author: MICEL
+"""
+
+def UPG (Gaji_per_Hari):
+    gaji = float (input (Gaji_per_Hari))
+    return gaji
+def UPL (Lembur_per_Jam):
+    lembur = float (input (Lembur_per_Jam))
+    return lembur
+
+Gaji_per_Hari = 175.000
+Lembur_per_Jam = 15.000
+
+Gaji_per_Hari = UPG ("Gaji per Hari = Rp")
+Lembur_per_Jam = UPL ("Lembur per Jam = Rp")
